fde12ffc463cbafe6d991ed38aff7ec8023856e3
erocheleau <etienne.rocheleau@gmail.com>
2016-10-07 10:18:59 -0400

    Cleaner + comments for example

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,15 @@
-
-
 var PushClient = require('./PushClient');
 
+// TOKEN TO CONNECT TO SLACK
 var token = process.env.SLACK_API_TOKEN || '';
+// TOKEN TO PUSH TO THE COVEO PUSH API
 var pushtoken = process.env.COVEO_PUSH_TOKEN || '';
+// ID OF THE COVEO ORGANIZATION TO PUSH TO
 var organizationId = process.env.COVEO_ORGANIZATIONID || '';
+// ID OF THE SOURCE TO PUSH TO
 var sourceId = process.env.COVEO_SOURCEID || '';
 
 
-// START
-
+// START THE SLACK PUSH CLIENT
 var myPushClient = new PushClient(token, organizationId, sourceId, pushtoken);
 myPushClient.start();
--- src/app/pushclient.js
+++ src/app/pushclient.js
@@ -1,3 +1,6 @@
+// View the slack nodejs library @ https://github.com/slackhq/node-slack-sdk
+// or https://www.npmjs.com/package/@slack/client
+
 var RtmClient = require('@slack/client').RtmClient;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
@@ -15,6 +18,7 @@ function PushClient(slacktoken, organizationId, sourceId, pushtoken) {
     this.bindEvents();
 }
 
+// Init the slack rtm client with a MemoryDataStore
 PushClient.prototype.init = function() {
     this.rtm = new RtmClient(this.slacktoken, {
         logLevel: 'error',
@@ -31,50 +35,64 @@ PushClient.prototype.handleAuthenticated = function(rtmStartData) {
     this.team = rtmStartData.team;
 }
 
+// This is called every time there's a new message visible to the bot on slack
 PushClient.prototype.handleNewMessage = function(message) {
     var _this = this;
 
-    var channel = _this.rtm.dataStore.getChannelGroupOrDMById(message.channel).name;
-    channel = (channel !== 'undefined') ? channel : message.channel;
-    var timeStr = message.ts.replace('.', '');
-    var documentId = `https://${_this.team.name}.slack.com/archives/${channel}/p${timeStr}`;
-    var body = _this.buildMessageBodyString(message);
+    // Build the body of the message containing all the metadata fields necessary
+    var document = _this.buildMessageDocument(message);
 
+    // Logging
     console.log(`Sending message from ${_this.rtm.dataStore.getUserById(message.user).name} 
                 in channel ${_this.rtm.dataStore.getChannelGroupOrDMById(message.channel).name} 
                 @ ${message.ts}`);
 
+    // Push the message as a document to the pushAPI
+    _this.pushDocument(JSON.stringify(document), document.documentId);
+}
+
+PushClient.prototype.pushDocument = function(documentBody, documentId) {
+    var _this = this;
+
+    var server = 'push.cloud.coveo.com'
+    var APIversion = 'v1'
+
+    // This sends a request to 
+    // https://push.cloud.coveo.com/v1/organizations/{orgID}/sources/{sourceID}/documents?documentId={documentID}
+    // With the body containing the document metadata we constructed from the message
     request({
-        url: `https://push.cloud.coveo.com/v1/organizations/${_this.organizationId}/sources/${_this.sourceId}/documents`,
+        url: `https://${server}/${APIversion}/organizations/${_this.organizationId}/sources/${_this.sourceId}/documents`,
         qs: { documentId: documentId },
         method: 'PUT',
         headers: {
             'content-type': 'application/json',
-            'Authorization': _this.pushtoken
+            'Authorization': 'Bearer ' + _this.pushtoken
         },
-        body: body
+        body: documentBody
     }, function(error, response, body) {
-        if (error) {
+        if (error) { // Handle errors here, for example you could retry X number of times
             console.error(error);
+        } else if (response.statusCode == 202) { // Success
+            console.log('Success!');
         }
     });
 }
 
-PushClient.prototype.handleReaction = function(reaction) {
-    console.log('Reaction added: ', JSON.stringify(reaction));
-}
-
-PushClient.prototype.handleReactionRemoved = function(reaction) {
-    console.log('Reaction removed: ', reaction);
-}
-
-PushClient.prototype.buildMessageBodyString = function(message) {
+PushClient.prototype.buildMessageDocument = function(message) {
     var _this = this;
+
+    // Get the channel name of the message
     var channel = _this.rtm.dataStore.getChannelGroupOrDMById(message.channel).name;
-    if (channel === 'undefined') channel = 'Direct Message';
+    channel = (channel === 'undefined') ? 'Direct Message' : channel; // Direct messages have a channel name = 'undefined'
+    var timeStr = message.ts.replace('.', '');
+
+    // Build a documentId which is the URI of a message (This format is the uri slack uses for it's archive)
+    var documentId = `https://${_this.team.name}.slack.com/archives/${channel}/p${timeStr}`;
+
     var user = _this.rtm.dataStore.getUserById(message.user).name || '';
 
     var body = {
+        documentId: documentId,
         message: message.text,
         Data: message.text,
         team: _this.team.name,
@@ -84,7 +102,7 @@ PushClient.prototype.buildMessageBodyString = function(message) {
         timestamp: message.ts
     }
 
-    return JSON.stringify(body);
+    return body;
 }
 
 PushClient.prototype.bindEvents = function() {
